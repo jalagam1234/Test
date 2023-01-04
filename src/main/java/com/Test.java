346b8e9a51df647f1005722b2fafa0b84ef40112
jayaram12345 <96493645+jayaram12345@users.noreply.github.com>
2023-01-04 06:35:20 +0530

    Test.java added

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Test {
+
+	public void add() {
+		System.out.println("KKKKKKKKKKK---------->");
+		System.out.println("DDDDDDDDDD---------->");
+		System.out.println("GGGGGGGGGG--------->");
+	}
+}
