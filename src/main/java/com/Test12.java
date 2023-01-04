6cf83b91e77d7ab8fca6f8a4ac77bd6e02eeaf25
jayaram12345 <96493645+jayaram12345@users.noreply.github.com>
2023-01-04 07:49:12 +0530

    Message1

---
+++ src/main/java/com/Test12.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Test12 {
+
+	public void add() {
+		System.out.println("KKKKKKKKKKK---------->");
+		System.out.println("DDDDDDDDDD---------->");
+		System.out.println("GGGGGGGGGG--------->");
+	}
+}
