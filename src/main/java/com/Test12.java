34cc6793be56c7be1396918985e4b44fb4896495
jalagam1234 <55870490+jalagam1234@users.noreply.github.com>
2023-01-04 08:03:37 +0530

    Update Test12.java

--- src/main/java/com/Test12.java
+++ src/main/java/com/Test12.java
@@ -3,8 +3,6 @@ package com;
 public class Test12 {
 
 	public void add() {
-		System.out.println("KKKKKKKKKKK---------->");
-		System.out.println("DDDDDDDDDD---------->");
-		System.out.println("GGGGGGGGGG--------->");
+		System.out.println("KKKKKKKKKKK---------->");		
 	}
 }
