f3b94621713f3f40c0a51ef529315dc81abbe588
jayaram12345 <96493645+jayaram12345@users.noreply.github.com>
2023-01-04 06:33:37 +0530

    initial

---
+++ src/main/java/com/App.java
@@ -0,0 +1,15 @@
+package com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! ...kkkk." );
+        
+         System.out.println( "KKKKKKKKKKKKKKKK---------->  " );
+    }
+}
